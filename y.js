24b6d013f11ff5df881c153ab42a3fd0fa7b2cfc
Alex Konrad <alexkonrad08@gmail.com>
2016-08-17 22:27:52 -0700

    remove logging

--- y.js
+++ y.js
@@ -1,18 +1,11 @@
-var l = console.log
-
 // Y combinator
 var Y = function Y (f) {
-  l(Y, f)
   return ((function A (x) {
-    l(A, x)
     return f(function B(v) {
-      l(B, x(x)(v))
       return x(x)(v)
     });
   })(function C (x) {
-    l(C, x)
     return f(function D (v) {
-      l(D, v, x(x)(v))
       return x(x)(v)
     })
   }))
